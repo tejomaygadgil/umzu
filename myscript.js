3d7cc9a34d7dbc7da37b7f1d59148e2c763ff664
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-12 15:36:07 -0800

    Lint

--- myscript.js
+++ myscript.js
@@ -1,13 +1,12 @@
 
-$(document).click(function(event) {
-    var element = $(event.target).parent().closest('.flowchart-label')
-    if (element.length > 0) {
-        node = element[0].id.split('-')[1]
-        console.log(node)
-        document.getElementById("node").innerHTML = node
-        }
+$(document).click(function (event) {
+  var element = $(event.target).parent().closest('.flowchart-label')
+  if (element.length > 0) {
+    node = element[0].id.split('-')[1]
+    document.getElementById("node").innerHTML = node
+  }
 });
 
 function updateState() {
-    document.getElementById("state").innerHTML = document.getElementById("state_select").value
+  document.getElementById("state").innerHTML = document.getElementById("state_select").value
 }
