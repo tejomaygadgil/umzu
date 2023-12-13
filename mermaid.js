372506b71bfe4477e4e7219ac6b1f770740b3b45
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-12 17:40:08 -0800

    Dynamically generate mermaid graph from text file

--- mermaid.js
+++
@@ -1,7 +0,0 @@
-mermaid`
-graph TD 
-    A[Client] --> B[Load Balancer] 
-    B --> C[Server1] 
-    B --> D[Server2]
-`
-
--- myscript.js
+++ myscript.js
@@ -1,11 +1,29 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+
 $(document).click(function (event) {
-  var element = $(event.target).parent().closest('.flowchart-label')
+  var element = $(event.target).parent().closest('.flowchart-label');
   if (element.length > 0) {
-    node = element[0].id.split('-')[1]
-    document.getElementById("node").innerHTML = node
-  }
+    var node = element[0].id.split('-')[1];
+    document.getElementById("node").innerHTML = node;
+  };
 });
 
-function updateState() {
-  document.getElementById("state").innerHTML = document.getElementById("state_select").value
-}
+$("#state_select").change(function(){
+  var selectedState = this.value;
+  document.getElementById("state").innerHTML =  selectedState;
+});
+
+// https://github.com/mermaid-js/mermaid/issues/2162 
+mermaid.initialize({ startOnLoad: false });
+const drawDiagram = async function () {
+  const element = document.querySelector('#graph');
+
+  // Read from file
+  const response = await fetch('graph.txt');
+  const graphDefinition = await response.text();
+  
+  const { svg } = await mermaid.render('mySvgId', graphDefinition);
+  element.innerHTML = svg;
+};
+await drawDiagram();
+
