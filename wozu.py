51ffdc70979c2525ab679325ddd23592cf5bd385
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-13 18:24:48 -0800

    Add process graph.txt function

--- wozu.py
+++ wozu.py
@@ -1,3 +1,20 @@
+def process_graph():
+    """
+    Read graph.txt and return nodes.
+    """
+    with open("graph.txt", "r") as f:
+        text = f.read().splitlines()
+
+    nodes = set()
+    for i, line in enumerate(text):
+        if line:
+            line_nodes = line.split(' --> ')
+            assert len(line_nodes) == 2, f"Malformed: line {i}, '{line}"
+            nodes.update(line_nodes)
+
+    return text, nodes
+
+
 if __name__ == "__main__":
     states = [
         "Want to do",
@@ -20,3 +37,9 @@ if __name__ == "__main__":
         "green": "#4CBB17",
         "purple": "#702963",
     }
+
+    # Handle graph.txt
+    graph_text, graph_nodes = process_graph()
+
+
+    print(graph_nodes)
