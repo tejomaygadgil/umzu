21053c547a367080fb214ea3d3d5569a66256610
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-13 20:04:05 -0800

    Successfully generate HTML (!)

--- wozu.py
+++ wozu.py
@@ -1,15 +1,11 @@
 states = [
-    "want to do",
     "will do",
     "doing",
-    "done",
 ]
 
 colors = [
-    "",
-    "#EE4B2B",
-    "#4CBB17",
-    "#702963",
+    "#0047AB", # Blue
+    "#EE4B2B", # Red
 ]
 
 state_to_color = dict(zip(states, colors))
@@ -46,19 +42,24 @@ def read_state():
     raw_text = read_file("todo.txt")
 
     current_state = None
-    states = dict()
+    states_map = dict()
     nodes = set()
     for i, line in enumerate(raw_text):
         if line:
             if line[0] == "#":
-                current_state = line[1:].strip()
-                states[current_state] = []
+                state = line[1:].strip().lower()
+                if state in states:
+                    current_state = state
+                    states_map[current_state] = []
+                else:
+                    current_state = None
             else:
-                node = line.strip()
-                states[current_state].append(node)
-                nodes.add(node)
+                if current_state:
+                    node = line.strip()
+                    states_map[current_state].append(node)
+                    nodes.add(node)
 
-    return states, nodes
+    return states_map, nodes
 
 def read_links():
     """
@@ -90,10 +91,44 @@ if __name__ == "__main__":
     # Consolidate nodes
     nodes = sorted(set.union(*[graph_nodes, state_nodes, links_nodes]))
 
-    """
-    text = []
+    # Create HTML
+    above = ["""
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="#">
+  <title>Wozu</title>
+</head>
+
+<body>
+  <div id="graph" class="mermaid">
+    flowchart LR
+    
+    """]
+
+    below = ["""
+</div>
+  <script type="module">
+    import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+    mermaid.initialize({ startOnLoad: true });
+  </script>
+</body>
+
+</html>
+    """]
+
+    state_text = []
     for state, nodes in states.items():
         color = state_to_color[state]
         for node in nodes:
-            text.append(f"style {node} fill
-    """
+            state_text.append(f"style {node} fill:{color}")
+            state_text.append(f"style {node} color:#FFFFFF")
+
+    html = "\n".join(above + graph_text + state_text + links_text + below)
+    
+    # Export
+    with open("index.html", "w") as f:
+        f.write(html)
