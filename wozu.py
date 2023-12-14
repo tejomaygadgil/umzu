62a03d3e3b4408021080e28ea53dc3b07b1d5441
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-13 17:53:15 -0800

    Init wozu.py and add state/color map

---
+++ wozu.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+    states = [
+        "Want to do",
+        "Will do",
+        "Doing",
+        "Done",
+    ]
+
+    colors = [
+        "",
+        "yellow",
+        "red",
+        "purple",
+    ]
+
+    state_to_color = dict(zip(states, colors))
+
+    color_map = {
+        "red": "#EE4B2B",
+        "green": "#4CBB17",
+        "purple": "#702963",
+    }
